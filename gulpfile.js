5bf3bd1d5722a5ed66258f25614e606a508fb984
kusya <tizahodieslichto@gmail.com>
2019-12-26 14:45:43 +0300

    delete project Tatarhata, fix favicon, edit Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,120 @@
-const gulp = require("gulp");
+const {
+    src,
+    dest,
+    watch,
+    parallel,
+    series
+} = require('gulp');
 const browserSync = require("browser-sync");
 const sass = require("gulp-sass");
 const rename = require("gulp-rename");
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const del = require('del');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+const ghPages = require('gulp-gh-pages-with-updated-gift');
+const svgSprite = require('gulp-svg-sprite');
 
-gulp.task("server", function () {
+const path = {
+    dist: {
+        html: 'dist/',
+        js: 'dist/js/',
+        css: 'dist/css/',
+        img: 'dist/img/',
+        icons: 'dist/icons/',
+        favicon: 'dist/favicon'
+    },
+    app: {
+        html: ['app/*.html', 'app/*.ico'],
+        js: 'app/js/*.js',
+        scss: 'app/sass/*.+(scss|sass)',
+        css: 'app/css/*.css',
+        img: 'app/img/**/*.*',
+        icons: 'app/icons/**/*.svg',
+        favicon: 'app/favicon/*.*'
+
+    },
+    clean: './dist/',
+    deploy: 'dist/**/*'
+}
+
+function favicon() {
+    return src(path.app.favicon).pipe(dest(path.dist.favicon));
+}
+
+function svg() {
+    return src(path.app.icons)
+        .pipe(svgSprite( {
+            mode: {
+                stack: {
+                    sprite: "../sprite.svg"
+                }
+            },
+        })
+    )
+    .pipe(dest(path.dist.icons));
+}
+
+
+function js() {
+    return src(path.app.js)
+        .pipe(concat('all.js'))
+        .pipe(terser())
+        .pipe(dest(path.dist.js)
+    );
+}
+
+async function clean() {
+    return del.sync(path.clean);
+}
+
+function images() {
+    return src(path.app.img).pipe(cache(imagemin([
+            imagemin.gifsicle({
+                interlaced: true
+            }),
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 5
+            }),
+            imagemin.svgo({
+                plugins: [{
+                        removeViewBox: true
+                    },
+                    {
+                        cleanupIDs: false
+                    }
+                ]
+            })
+        ])))
+        .pipe(dest(path.dist.img));
+}
+
+function html() {
+    return src(path.app.html).pipe(dest(path.dist.html));
+}
+
+
+function server() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: path.dist.html
         }
     });
-});
+}
 
-gulp.task("styles", function () {
-    return gulp
-        .src("src/sass/*.+(scss|sass)")
-        .pipe(sass({
-            outputStyle: "compressed"
-        }).on("error", sass.logError))
+
+function styles() {
+    return src(path.app.scss).pipe(
+            sass({
+                outputStyle: "compressed"
+            }).on("error", sass.logError)
+        )
         .pipe(
             rename({
                 prefix: "",
@@ -30,16 +126,26 @@ gulp.task("styles", function () {
                 cascade: false
             })
         )
-        .pipe(cleanCSS({
-            compatibility: "ie8"
-        }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(
+            cleanCSS({
+                compatibility: "ie8"
+            })
+        )
+        .pipe(dest(path.dist.css))
         .pipe(browserSync.stream());
-});
+}
 
-gulp.task("watch", function () {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
-});
+function look() {
+    watch(path.app.scss, parallel(styles));
+    watch(path.app.js, parallel(js));
+    watch(path.app.html).on("change", series(html, browserSync.reload));
+}
 
-gulp.task("default", gulp.parallel("watch", "server", "styles"));
+function deploy() {
+    return src(path.deploy)
+        .pipe(ghPages());
+}
+
+exports.build = series(clean, parallel(favicon, styles, images, svg, js, html));
+exports.default = series(exports.build, parallel(look, server));
+exports.deploy = series(exports.build, deploy);
